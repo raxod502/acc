3ea4b6bd70321d587a1c93455109c58d7a722939
Radon Rosborough <radon.neon@gmail.com>
2017-07-29 20:37:06 -0700

    Add some basic classes

---
+++ .dir-locals.el
@@ -0,0 +1,1 @@
+((python-mode . ((flycheck-python-pycompile-executable . "python3"))))
---
+++ acc.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+def make_repr(self, keys):
+    classname = self.__class__.__name__
+    keyvals = []
+    for key in keys:
+        keyvals.append(f"{key}={repr(getattr(self, key))}")
+    body = ", ".join(keyvals)
+    return f"{classname}({body})"
+
+class Transaction:
+    def __init__(self, id_, date=None, description=None, category=None):
+        self.id_ = id_
+        self.date = date
+        self.description = description
+        self.category = category
+
+    def __repr__(self):
+        return make_repr(self, ['id_', 'date', 'description', 'category'])
+
+class CreditOrDebit(Transaction):
+    def __init__(self, id_, account, value, date=None, description=None,
+                 category=None):
+        super().__init__(id_=id_, date=date, description=description,
+                         category=category)
+        self.account = account
+        self.value = value
+
+    def __repr__(self):
+        return make_repr(self, ['id_', 'account', 'value', 'date',
+                                'description', 'category'])
+
+class Credit(CreditOrDebit):
+    def __init__(self, id_, account, value, date=None, description=None,
+                 category=None):
+        super().__init__(id_=id_, account=account, value=value, date=date,
+                         description=description, category=category)
+
+class Debit(CreditOrDebit):
+    def __init__(self, id_, account, value, date=None, description=None,
+                 category=None):
+        super().__init__(id_=id_, account=account, value=-value, date=date,
+                         description=description, category=category)
+
+class Transfer(Transaction):
+    def __init__(self, id_, source_account, target_account, value,
+                 date=None, description=None, category=None):
+        super().__init__(id_=id_, date=date, description=description,
+                         category=category)
+        self.source_account = source_account
+        self.target_account = target_account
+        self.value = value
+
+    def __repr__(self):
+        return make_repr(self, ['id_', 'source_account', 'target_account',
+                                'value', 'date', 'description', 'category'])
+
+class Account:
+    def __init__(self, name):
+        self.name = name
