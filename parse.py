9bcad1a9c0d8463e204feec6b96394dbd703389a
Radon Rosborough <radon.neon@gmail.com>
2017-07-30 00:20:41 -0700

    Add token group interpreter

--- parse.py
+++ parse.py
@@ -1,3 +1,5 @@
+import util
+
 # Note: it is assumed that nothing in DELIMITER_CHARS or ESCAPE_CHARS
 # is whitespace.
 
@@ -11,10 +13,10 @@ DELIMITER_CHARS = {
 ESCAPE_CHARS = ["\\"]
 
 class TokenizationError(Exception):
-    def __init__(self, message, index=None, *args, context=None):
+    def __init__(self, message, index=None, *args, content=None):
         super().__init__(message, *args)
         self.index = index
-        self.context = context
+        self.content = content
 
 def read_token(string, parsing_start_index):
     found_token = False
@@ -44,7 +46,7 @@ def read_token(string, parsing_start_index):
                 escape_sequence_index = index - 1
                 raise TokenizationError("Malformed escape sequence",
                                         index=escape_sequence_index,
-                                        context=escape_sequence)
+                                        content=escape_sequence)
             token_chars.append(char)
             escape_next_char = False
         elif char == end_delimiter:
@@ -63,12 +65,12 @@ def read_token(string, parsing_start_index):
         escape_sequence_index = index - 1
         raise TokenizationError("Unfinished escape sequence",
                                 index=escape_sequence_index,
-                                context=unfinished_escape_sequence)
+                                content=unfinished_escape_sequence)
     if end_delimiter:
         unfinished_token = string[token_start_index:]
         raise TokenizationError("Unfinished quoted token",
                                 index=token_start_index,
-                                context=unfinished_token)
+                                content=unfinished_token)
     return token_end_index, "".join(token_chars) if found_token else None
 
 def read_tokens(string):
@@ -79,3 +81,89 @@ def read_tokens(string):
         if token:
             tokens.append(token)
     return tokens
+
+CLAUSE_NAMES = ["category", "date", "description", "from", "time", "to"]
+
+CLAUSE_PREFIXES = {
+    "category": ["and", "in", "using", "with"],
+    "date": ["and", "at", "on", "using", "with"],
+    "description": ["and", "using", "with"],
+    "from": ["and"],
+    "time": ["and", "at", "using", "with"],
+    "to": ["and"],
+}
+
+CLAUSE_SUFFIXES = {
+    "category": ["of"],
+    "date": ["of"],
+    "description": ["of"],
+    "from": ["account"],
+    "time": ["of"],
+    "to": ["account"],
+}
+
+def clause_arguments(clause_name, config):
+    # FIXME
+    return ["foo", "bar", "baz", "quux"]
+
+def matches(x, y):
+    return y.casefold().startswith(x.casefold())
+
+def matches_exactly(x, y):
+    return y.casefold() == x.casefold()
+
+class Clause:
+    def __init__(self, name, argument):
+        self.name = name
+        self.argument = argument
+
+    def __repr__(self):
+        return util.make_repr(self, ["name", "argument"])
+
+class GroupingError(Exception):
+    def __init__(self, message, *args, content=None):
+        super().__init__(message, *args)
+        self.content = content
+
+def interpret_token_group(tokens, config):
+    interpretations = []
+    for index, token in enumerate(tokens[:-1]):
+        for clause_name in CLAUSE_NAMES:
+            valid = True
+            if matches(token, clause_name):
+                for prefix in tokens[:index]:
+                    if not any(
+                            matches_exactly(prefix, clause_prefix)
+                            for clause_prefix in CLAUSE_PREFIXES[clause_name]):
+                        valid = False
+                        break
+                if not valid:
+                    break
+                for suffix in tokens[index + 1:-1]:
+                    if not any(
+                            matches_exactly(suffix, clause_suffix)
+                            for clause_suffix in CLAUSE_SUFFIXES[clause_name]):
+                        valid = False
+                        break
+                if not valid:
+                    break
+                argument = tokens[-1]
+                for clause_argument in clause_arguments(clause_name, config):
+                    if matches(argument, clause_argument):
+                        interpretation = Clause(clause_name, clause_argument)
+                        interpretations.append(interpretation)
+    return interpretations
+
+def interpret_token_groups(tokens, config):
+    if not tokens:
+        return [[]]
+    interpretations = []
+    for length in range(2, len(tokens) + 1):
+        head_interpretations = interpret_token_group(tokens[:length], config)
+        if head_interpretations:
+            tail_interpretations = interpret_token_groups(
+                tokens[length:], config)
+            for head in head_interpretations:
+                for tail in tail_interpretations:
+                    interpretations.append([head] + tail)
+    return interpretations
