091ede8f07891e208ac3616b793f69e89643761e
Radon Rosborough <radon.neon@gmail.com>
2018-06-07 22:05:25 -0600

    Add top-level dispatch script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+# https://python-packaging.readthedocs.io/en/latest/minimal.html
+setup(
+    author="Radon Rosborough",
+    author_email="radon.neon@gmail.com",
+    description="Bare-bones command-line accounting.",
+    license="MIT",
+    install_requires=[],
+    name="acc",
+    scripts=["scripts/acc"],
+    url="https://github.com/raxod502/acc",
+    version="1.0",
+    zip_safe=True,
+)
